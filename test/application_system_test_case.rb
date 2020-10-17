27398205720b48de81e802292072535e9441fe82
PBtatsuya <tito@p-bank.org>
2020-10-18 02:44:12 +0900

    Finish static pages

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,25 @@
 require "test_helper"
 
-class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+  
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+  
 end
